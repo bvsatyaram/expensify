cc29aa9af5bce47e329c409326fc6b0b6858556d
BV Satyaram <bvsatyaram@gmail.com>
2017-12-13 14:34:33 +0530

    Built It - Visibility
    Lecture #21

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,34 +1,14 @@
 'use strict';
 
 var appData = {
-  title: 'Indecision App',
-  subTitle: 'Organize your work!',
-  options: ['Options 1', 'Options 2']
+  title: 'Visibility Toggle',
+  showDetails: false,
+  details: 'Hey. These are some details you can now see!'
 };
 
-var onFormSubmit = function onFormSubmit(e) {
-  e.preventDefault();
-
-  var option = e.target.elements.option.value;
-  if (option) {
-    appData.options.push(option);
-  }
-  e.target.elements.option.value = '';
+var toggleDeatils = function toggleDeatils() {
+  appData.showDetails = !appData.showDetails;
   renderApp();
-};
-
-var clearOptions = function clearOptions(e) {
-  e.preventDefault();
-
-  appData.options = [];
-  renderApp();
-};
-
-var pickOption = function pickOption(e) {
-  e.preventDefault();
-
-  var randNum = Math.floor(Math.random() * appData.options.length);
-  console.log(appData.options[randNum]);
 };
 
 var appRoot = document.getElementById('app');
@@ -43,42 +23,14 @@ var renderApp = function renderApp() {
       appData.title
     ),
     React.createElement(
+      'button',
+      { onClick: toggleDeatils },
+      appData.showDetails ? 'Hide details' : 'Show details'
+    ),
+    appData.showDetails && React.createElement(
       'p',
       null,
-      appData.subTitle
-    ),
-    React.createElement(
-      'ul',
-      null,
-      appData.options.map(function (option, index) {
-        return React.createElement(
-          'li',
-          { key: 'option-' + index },
-          option
-        );
-      })
-    ),
-    React.createElement(
-      'form',
-      { onSubmit: onFormSubmit },
-      React.createElement('input', { type: 'text', name: 'option', autoFocus: true }),
-      React.createElement(
-        'button',
-        { type: 'submit' },
-        'Add Option'
-      ),
-      React.createElement(
-        'button',
-        { onClick: clearOptions },
-        'Remove All'
-      ),
-      React.createElement('br', null),
-      React.createElement('br', null),
-      React.createElement(
-        'button',
-        { onClick: pickOption },
-        'What Should I do?'
-      )
+      appData.details
     )
   );
 
