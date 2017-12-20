a7180140bcb5d52a9a9a4b76538c849004817722
BV Satyaram <bvsatyaram@gmail.com>
2017-12-21 05:06:35 +0530

    Import/Export with webpack
    Lecture: #52

--- public/bundle.js
+++ public/bundle.js
@@ -65,9 +65,30 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-console.log('Webpack is running!!');
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__utils__ = __webpack_require__(1);
+
+
+[17, 18, 19, 20, 21, 22].forEach((age) => {
+  console.log(`${age} year old is an adult: ${Object(__WEBPACK_IMPORTED_MODULE_0__utils__["b" /* isAdult */])(age)}`);
+  console.log(`${age} year old can drink: ${Object(__WEBPACK_IMPORTED_MODULE_0__utils__["a" /* default */])(age)}`);
+})
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return isAdult; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return canDrink; });
+const isAdult = (age) => (age > 18);
+
+const canDrink = (age) => (age > 21);
+
+
 
 
 /***/ })
--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-console.log('Webpack is running!!');
+import canDrink, {isAdult} from './utils';
+
+[17, 18, 19, 20, 21, 22].forEach((age) => {
+  console.log(`${age} year old is an adult: ${isAdult(age)}`);
+  console.log(`${age} year old can drink: ${canDrink(age)}`);
+})
