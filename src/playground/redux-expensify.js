38e306b34291a0d6003499ae88521e8c35095dd4
BV Satyaram <bvsatyaram@gmail.com>
2018-01-01 04:37:58 +0530

    Expeses reducers
    Lecture: #94

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,4 +1,34 @@
 import {createStore, combineReducers} from 'redux';
+import uuid from 'uuid';
+
+// Expense Action Generators
+const addExpense = (
+  {
+    description = '',
+    note = '',
+    amount = 0,
+    createdAt = 0
+  } = {}
+) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
+
+const removeExpense = (expenseId) => ({
+  type: 'REMOVE_EXPENSE',
+  expenseId
+});
+
+const editExpense = (expense) => ({
+  type: 'EDIT_EXPENSE',
+  expense
+});
 
 // Expenses Reducer
 
@@ -6,6 +36,21 @@ const expenseReducerDefaultState = [];
 
 const expenseReducer = (state = expenseReducerDefaultState, action) => {
   switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [...state, action.expense];
+    case 'REMOVE_EXPENSE':
+      return state.filter((expense) => (expense.id !== action.expenseId));
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.expense.id) {
+          return {
+            ...expense,
+            ...action.expense
+          };
+        } else {
+          return expense;
+        }
+      });
     default:
       return state;
   }
@@ -30,4 +75,15 @@ const store = createStore(combineReducers({
   filters: filtersReducer
 }));
 
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+})
+
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 2500 }));
+store.dispatch(addExpense({ description: 'Coffee', amount: 300 }));
+
+// store.dispatch(removeExpense(expenseOne.expense.id));
+store.dispatch(editExpense({
+  id: expenseOne.expense.id,
+  amount: 3500
+}));
